f6911c545042be36bdc81dda9b9d02f8a6b53804
eitsupi <50911393+eitsupi@users.noreply.github.com>
2022-10-31 23:54:47 +0900

    [go-task] Fix permission of user home when install pwsh completion (#21)

--- src/go-task/install.sh
+++ src/go-task/install.sh
@@ -152,8 +152,9 @@ setup_completions() {
         mkdir -p "$pwsh_profile_dir"
         mv "$pwsh_comp_file" "${pwsh_script_dir}/task.ps1"
         echo "Import-Module ${pwsh_script_dir}/task.ps1" >>"$pwsh_profile_file"
-        chown -R "${USERNAME}:${USERNAME}" "$pwsh_script_dir"
-        chown -R "${USERNAME}:${USERNAME}" "$pwsh_profile_dir"
+        if [ "$USERNAME" != "root" ]; then
+            chown -R "${USERNAME}:${USERNAME}" "/home/${USERNAME}"
+        fi
     fi
 }
 
