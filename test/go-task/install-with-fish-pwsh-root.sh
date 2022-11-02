68965e84fb7e3116b80eee4b5e3a7512346cd1b8
eitsupi <50911393+eitsupi@users.noreply.github.com>
2022-11-02 19:21:13 +0900

    [go-task] Fix pwsh profile's path (#23)

--- test/go-task/install-with-fish-pwsh-root.sh
+++ test/go-task/install-with-fish-pwsh-root.sh
@@ -9,6 +9,7 @@ source dev-container-features-test-lib
 check "version" task --version | grep "$LATEST_VERSION"
 check "fish completion" cat "/root/.config/fish/completions/task.fish"
 check "pwsh completion" cat "/root/.local/share/powershell/Scripts/task.ps1"
+check "pwsh profile" cat "/root/.config/powershell/Microsoft.PowerShell_profile.ps1" | grep "task.ps1"
 
 # Report result
 reportResults
