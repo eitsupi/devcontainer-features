a9f2c67b38f41c18def6b0b971b21093a8e8d628
eitsupi <50911393+eitsupi@users.noreply.github.com>
2022-11-06 13:16:37 +0900

    [go-task] Remove unused lines from tests (#32)

--- test/go-task/install-with-fish-pwsh-root.sh
+++ test/go-task/install-with-fish-pwsh-root.sh
@@ -6,7 +6,6 @@ set -e
 source dev-container-features-test-lib
 
 # Feature-specific tests
-check "version" task --version | grep "$LATEST_VERSION"
 check "fish completion" cat "/root/.config/fish/completions/task.fish"
 check "pwsh completion" cat "/root/.local/share/powershell/Scripts/task.ps1"
 check "pwsh profile" cat "/root/.config/powershell/Microsoft.PowerShell_profile.ps1" | grep "task.ps1"
