fbde795cb3ec9a61aa89fed9c39cec6de35dd68a
eitsupi <ts1s1andn@gmail.com>
2022-11-06 03:42:49 +0000

    fix missing argument

--- src/jq-likes/install.sh
+++ src/jq-likes/install.sh
@@ -4,6 +4,8 @@ JQ_VERSION=${JQVERSION:-"os-provided"}
 YQ_VERSION=${YQVERSION:-"none"}
 GOJQ_VERSION=${GOJQVERSION:-"none"}
 
+USERNAME=${USERNAME:-"automatic"}
+
 set -e
 
 # Clean up
@@ -18,6 +20,23 @@ architecture="$(dpkg --print-architecture)"
 if [ "${architecture}" != "amd64" ] && [ "${architecture}" != "arm64" ]; then
     echo "(!) Architecture $architecture unsupported"
     exit 1
+fi
+
+# Determine the appropriate non-root user
+if [ "${USERNAME}" = "auto" ] || [ "${USERNAME}" = "automatic" ]; then
+    USERNAME=""
+    POSSIBLE_USERS=("vscode" "node" "codespace" "$(awk -v val=1000 -F ":" '$3==val{print $1}' /etc/passwd)")
+    for CURRENT_USER in "${POSSIBLE_USERS[@]}"; do
+        if id -u "${CURRENT_USER}" >/dev/null 2>&1; then
+            USERNAME=${CURRENT_USER}
+            break
+        fi
+    done
+    if [ "${USERNAME}" = "" ]; then
+        USERNAME=root
+    fi
+elif [ "${USERNAME}" = "none" ] || ! id -u ${USERNAME} >/dev/null 2>&1; then
+    USERNAME=root
 fi
 
 apt_get_update() {
@@ -154,7 +173,7 @@ if [ "${YQ_VERSION}" != "none" ]; then
     ./install-man-page.sh
     popd
     rm -rf /tmp/yq
-    setup_yq_completions
+    setup_yq_completions "${USERNAME}"
 fi
 
 if [ "${GOJQ_VERSION}" != "none" ]; then
