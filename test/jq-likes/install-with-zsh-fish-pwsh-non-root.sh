0aaeac5d3683747a00b7b9863fb2ceb09b28ab06
eitsupi <50911393+eitsupi@users.noreply.github.com>
2022-12-11 13:01:31 +0000

    update tests

--- test/jq-likes/install-with-zsh-fish-pwsh-non-root.sh
+++ test/jq-likes/install-with-zsh-fish-pwsh-non-root.sh
@@ -2,27 +2,16 @@
 
 set -e
 
-NON_ROOT_USER=""
-POSSIBLE_USERS=("vscode" "node" "codespace" "$(awk -v val=1000 -F ":" '$3==val{print $1}' /etc/passwd)")
-for CURRENT_USER in "${POSSIBLE_USERS[@]}"; do
-  if id -u "${CURRENT_USER}" >/dev/null 2>&1; then
-    NON_ROOT_USER="${CURRENT_USER}"
-    break
-  fi
-done
-if [ "${NON_ROOT_USER}" = "" ]; then
-  NON_ROOT_USER=root
-fi
-
 # shellcheck source=/dev/null
 source dev-container-features-test-lib
 
 # Feature-specific tests
-check "yq zsh completion" cat "/usr/local/share/zsh/site-functions/_yq"
-check "yq fish completion" cat "/home/${NON_ROOT_USER}/.config/fish/completions/yq.fish"
-check "yq pwsh completion" cat "/home/${NON_ROOT_USER}/.local/share/powershell/Scripts/yq.ps1"
-check "yq pwsh profile" cat "/home/${NON_ROOT_USER}/.config/powershell/Microsoft.PowerShell_profile.ps1" | grep "yq.ps1"
-check "gojq zsh completion" cat "/usr/local/share/zsh/site-functions/_gojq"
+check "ensure i am user vscode"  bash -c "whoami | grep 'vscode'"
+check "yq zsh completion" cat /usr/local/share/zsh/site-functions/_yq
+check "yq fish completion" cat ~/.config/fish/completions/yq.fish
+check "yq pwsh completion" cat ~/.local/share/powershell/Scripts/yq.ps1
+check "yq pwsh profile" cat ~/.config/powershell/Microsoft.PowerShell_profile.ps1 | grep "yq.ps1"
+check "gojq zsh completion" cat /usr/local/share/zsh/site-functions/_gojq
 
 # Report result
 reportResults
