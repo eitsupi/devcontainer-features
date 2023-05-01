ed05fb3e0913a1b845768fa1c54786ca1ed239bf
Pete Fein <pete@snake.dev>
2023-05-01 09:24:02 -0400

    [duckdb-cli] Workaround for duckdb using aarch64 to mean arm64 (#70)
    This feature does not currently work correctly on `arm64` architectures
    (such as Mac M1) because duckdb calls that `aarch64`. Work around that.
    
    Also change curl parameters to fail on 404, instead of creating an
    invalid zip file
    
    ---------
    
    Co-authored-by: eitsupi <ts1s1andn@gmail.com>

--- src/duckdb-cli/install.sh
+++ src/duckdb-cli/install.sh
@@ -118,7 +118,9 @@ echo "Downloading DuckDB CLI..."
 
 mkdir /tmp/duckdb-cli
 pushd /tmp/duckdb-cli
-curl -sL "https://github.com/duckdb/duckdb/releases/download/v${CLI_VERSION}/duckdb_cli-linux-$(dpkg --print-architecture).zip" -o duckdb_cli.zip
+duckdb_arch=$(dpkg --print-architecture)
+if [ "${duckdb_arch}" = "arm64" ] ; then duckdb_arch="aarch64" ; fi
+curl -fL --no-progress-meter "https://github.com/duckdb/duckdb/releases/download/v${CLI_VERSION}/duckdb_cli-linux-${duckdb_arch}.zip" -o duckdb_cli.zip
 
 unzip duckdb_cli.zip
 mv duckdb /usr/bin/duckdb
