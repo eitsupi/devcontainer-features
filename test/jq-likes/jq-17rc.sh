71fd3d5d3b21ca29633dd919208cddb679f34eb9
eitsupi <50911393+eitsupi@users.noreply.github.com>
2023-08-01 14:16:41 +0000

    test: ensure check the version

--- test/jq-likes/jq-17rc.sh
+++ test/jq-likes/jq-17rc.sh
@@ -6,7 +6,7 @@ set -e
 source dev-container-features-test-lib
 
 # Feature-specific tests
-check "version" jq --version
+check "version" bash -c "jq --version | grep -q 'jq-1.7rc1'"
 
 # Report result
 reportResults
