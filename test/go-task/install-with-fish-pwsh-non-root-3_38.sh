a86f93a99b81c60ff821ce903298ffca702a7b61
eitsupi <50911393+eitsupi@users.noreply.github.com>
2024-09-09 11:15:42 +0000

    feat(go-task): support new `task --completion <shell>` command

---
+++ test/go-task/install-with-fish-pwsh-non-root-3_38.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -e
+
+# shellcheck source=/dev/null
+source dev-container-features-test-lib
+
+# Feature-specific tests
+check "version" bash -c "task --version | grep 3\.38"
+check "ensure i am user vscode"  bash -c "whoami | grep 'vscode'"
+check "fish completion" cat ~/.config/fish/completions/task.fish
+check "pwsh completion" cat ~/.local/share/powershell/Scripts/task.ps1
+check "pwsh profile" cat ~/.config/powershell/Microsoft.PowerShell_profile.ps1 | grep "task.ps1"
+
+# Report result
+reportResults
---
+++ test/go-task/install-with-fish-pwsh-root-3_38.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -e
+
+# shellcheck source=/dev/null
+source dev-container-features-test-lib
+
+# Feature-specific tests
+check "version" bash -c "task --version | grep 3\.38"
+check "fish completion" cat "/root/.config/fish/completions/task.fish"
+check "pwsh completion" cat "/root/.local/share/powershell/Scripts/task.ps1"
+check "pwsh profile" cat "/root/.config/powershell/Microsoft.PowerShell_profile.ps1" | grep "task.ps1"
+
+# Report result
+reportResults
