55000c5271ae7f170cf01c1a286e9b8c54cfb136
eitsupi <50911393+eitsupi@users.noreply.github.com>
2025-06-03 03:29:57 +0000

    test(jq-likes): Update test to support arm64

--- test/jq-likes/yq-gojq-xq-jaq-specific-version.sh
+++ test/jq-likes/yq-gojq-xq-jaq-specific-version.sh
@@ -7,7 +7,7 @@ source dev-container-features-test-lib
 
 # Feature-specific tests
 check "version" bash -c 'yq --version | grep "4.24.5"'
-check "gojq version" bash -c 'gojq --version | grep "0.7.0"'
+check "gojq version" bash -c 'gojq --version | grep "0.12.12"'
 check "xq version" bash -c 'xq --version | grep "0.2"'
 check "jaq version" bash -c 'jaq --version | grep "1"'
 
